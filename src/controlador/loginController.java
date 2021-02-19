dcaef65c8f3ac0182edb8a6f4bb51e74465beb7b
Mikkel-14 <jose.munoz01@epn.edu.ec>
2021-02-19 17:54:04 -0500

    LoginController: Inclusion del inicio de sesion para estudiantes

--- src/controlador/loginController.java
+++ src/controlador/loginController.java
@@ -10,60 +10,61 @@ import javax.servlet.http.HttpSession;
 
 import modelo.dao.DAOFactory;
 import modelo.entidad.Docente;
+import modelo.entidad.Estudiante;
 import modelo.jpa.JPAFactory;
 
 @WebServlet("/loginController")
 public class loginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		procesarSolicitud(request, response);
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		procesarSolicitud(request, response);
 	}
 
-	private void procesarSolicitud(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
-		
-		
-		
+	private void procesarSolicitud(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException, ServletException {
 		String usuario = req.getParameter("usuario");
 		String password = req.getParameter("password");
-		//admin //admin2021
-		
-		if(usuario.equals("admin") && password.equals("admin2021")){
-			//getServletContext().getRequestDispatcher("/ModuloAdministrador.jsp").forward(req, resp);
+		// admin //admin2021
+		DAOFactory fabrica = new JPAFactory();
+		Docente docente = (Docente) fabrica.crearUsuarioDAO(JPAFactory.DOCENTE).autorizar(usuario, password);
+		Estudiante estudiante = (Estudiante) fabrica.crearUsuarioDAO(JPAFactory.ESTUDIANTE).autorizar(usuario, password);
+
+		if (usuario.equals("admin") && password.equals("admin2021")) {
+			// getServletContext().getRequestDispatcher("/ModuloAdministrador.jsp").forward(req,
+			// resp);
 			HttpSession sesion = req.getSession();
 			sesion.setAttribute("usuario", usuario);
 			String tipo = "admin";
 			sesion.setAttribute("tipo", tipo);
-			//Navego hacia el JSP
+			// Navego hacia el JSP
 			getServletContext().getRequestDispatcher("/ModuloAdministrador.jsp").forward(req, resp);
-			
-			
+
+		} else if (docente != null) {
+
+			HttpSession sesion = req.getSession();
+			sesion.setAttribute("usuario", usuario);
+			String tipo = "docente";
+			sesion.setAttribute("tipo", tipo);
+			// Navego hacia el JSP
+			getServletContext().getRequestDispatcher("/docentePasswd.jsp").forward(req, resp);
+
+		} else if (estudiante != null) {
+			HttpSession sesion = req.getSession();
+			sesion.setAttribute("usuario", usuario);
+			sesion.setAttribute("tipo", "estudiante");
+			getServletContext().getRequestDispatcher("/moduloEstudiante.jsp").forward(req, resp);
+
 		} else {
-				DAOFactory fabrica = new JPAFactory();
-				Docente docente = (Docente)fabrica.crearUsuarioDAO(JPAFactory.DOCENTE).autorizar(usuario, password);
-			
-			if (docente != null){
-				HttpSession sesion = req.getSession();
-				sesion.setAttribute("usuario", usuario);
-				String tipo = "docente";
-				sesion.setAttribute("tipo", tipo);
-				//Navego hacia el JSP
-				getServletContext().getRequestDispatcher("/docentePasswd.jsp").forward(req, resp);
-				
-			}else{
-				resp.sendRedirect("index.jsp");
-				
-			}
-		}
-			
-
+			resp.sendRedirect("index.jsp");
 		}
 
-		
 	}
 
-
+}
