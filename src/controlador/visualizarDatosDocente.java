f5e0adb1ca392fce7015789db759592d792b9bfa
Carolina Mendoza <carolina.mendoza@epn.edu.ec>
2021-02-27 22:57:32 -0500

    xd

--- src/controlador/visualizarDatosDocente.java
+++ src/controlador/visualizarDatosDocente.java
@@ -27,7 +27,7 @@ public class visualizarDatosDocente extends HttpServlet{
 	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+		getServletContext().getRequestDispatcher("/actualizarDocente.jsp").forward(request, response);
 	}
 	
 	
